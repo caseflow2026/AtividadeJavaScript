b7aabf5b73e7cab61a0baeee75f921fb526b2335
caseflow2026 <reichembak.bruno@gmail.com>
2026-06-29 20:16:01 -0300

    Add files via upload

---
+++ Atividade01.js
@@ -0,0 +1,50 @@
+// Objeto do produto
+
+const produto = {
+    nome: "Monitor",
+    categoria: "Informática",
+    preco: "899.90",
+    estoque: 5
+};
+
+produto.preco = Number(produto.preco);
+
+
+if (isNaN(produto.preco)) {
+    console.log("Preço inválido.");
+} else {
+
+    
+    function exibirProduto(produto) {
+        console.log(`Produto: ${produto.nome}`);
+        console.log(`Categoria: ${produto.categoria}`);
+        console.log(`Preço: R$ ${produto.preco.toFixed(2)}`);
+        console.log();
+
+        if (produto.estoque < 10) {
+            console.log("Estoque baixo");
+        } else {
+            console.log("Estoque adequado");
+        }
+
+        console.log();
+
+        console.log(Object.keys(produto));
+
+        console.log();
+
+        console.log("Propriedades e valores:");
+        Object.entries(produto).forEach(([chave, valor]) => {
+            console.log(`${chave}: ${valor}`);
+        });
+
+        console.log();
+
+        console.log("Tipos das informações:");
+        Object.entries(produto).forEach(([chave, valor]) => {
+            console.log(`${chave}: ${typeof valor}`);
+        });
+    }
+
+    exibirProduto(produto);
+}
---
+++ Atividade05.js
@@ -0,0 +1,44 @@
+// Objeto do evento
+const evento = {
+    nome: "Semana da Tecnologia",
+    local: "Auditório",
+    data: new Date("2026-08-20"),
+    participantes: ["Ana", "Carlos", "Marcos"]
+};
+
+function exibirEvento(evento) {
+    console.log(`Evento: ${evento.nome}`);
+    console.log(`Local: ${evento.local}`);
+    console.log(`Data: ${evento.data.toLocaleDateString("pt-BR")}`);
+    console.log(`Participantes: ${evento.participantes.length}`);
+
+    if (evento.participantes.length > 0) {
+        console.log("Há participantes cadastrados.");
+    } else {
+        console.log("Nenhum participante cadastrado");
+    }
+
+    console.log(`Local cadastrado: ${evento.hasOwnProperty("local")}`);
+
+    console.log("\nPropriedades e valores:");
+    Object.entries(evento).forEach(([chave, valor]) => {
+        console.log(`${chave}: ${valor}`);
+    });
+
+    console.log("\nTipos das informações:");
+    Object.entries(evento).forEach(([chave, valor]) => {
+        console.log(`${chave}: ${typeof valor}`);
+    });
+}
+
+exibirEvento(evento);
+
+
+// entrada 2
+
+const evento = {
+    nome: "Workshop Angular",
+    local: "Laboratório 5",
+    data: new Date("2026-09-10"),
+    participantes: []
+};
